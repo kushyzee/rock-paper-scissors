71b4cde57c542813a1d282b1b8b8f78a2186c9f7
Arinze Ogbuniba <niklaus.oa@gmail.com>
2024-02-03 09:59:56 +0100

    Display scores of both players on the page

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 const btnGroup = document.querySelectorAll("button");
 const resultOutput = document.querySelector("#output");
 
-let displayResult = document.createElement("p");
-resultOutput.appendChild(displayResult);
+let displayRoundOutcome = document.createElement("p");
+resultOutput.appendChild(displayRoundOutcome);
+
+let displayRoundWinner = document.createElement("p");
+resultOutput.appendChild(displayRoundWinner);
+
+let playerScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
 	const choice = ["Rock", "Paper", "Scissors"];
@@ -13,9 +19,6 @@ function getComputerChoice() {
 }
 
 function game(e) {
-	let playerScore = 0;
-	let computerScore = 0;
-
 	let playerSelection = e.target.textContent;
 	let computerSelection = getComputerChoice();
 	let result = playRound(playerSelection, computerSelection);
@@ -23,10 +26,10 @@ function game(e) {
 	// determine who wins each round
 	if (result === "playerWins") {
 		playerScore++;
-		console.log(`player: ${playerScore} computer: ${computerScore}`);
+		displayRoundWinner.textContent = `player: ${playerScore} computer: ${computerScore}`;
 	} else {
 		computerScore++;
-		console.log(`player: ${playerScore} computer: ${computerScore}`);
+		displayRoundWinner.textContent = `player: ${playerScore} computer: ${computerScore}`;
 	}
 
 	// determine winner
@@ -40,7 +43,7 @@ function game(e) {
 function playRound(player, computer) {
 	// if tie, replay the round
 	if (player === computer) {
-		displayResult.textContent = `you both chose ${player}. Replay round`;
+		displayRoundOutcome.textContent = `you both chose ${player}. Replay round`;
 		btnGroup.forEach((btn) => {
 			btn.addEventListener("click", game);
 		});
@@ -48,19 +51,19 @@ function playRound(player, computer) {
 
 	// if player wins
 	else if (player === "Rock" && computer === "Scissors") {
-		displayResult.textContent = "You Win! Rock beats Scissors";
+		displayRoundOutcome.textContent = "You Win! Rock beats Scissors";
 		return "playerWins";
 	} else if (player === "Paper" && computer === "Rock") {
-		displayResult.textContent = "You Win! Paper beats Rock";
+		displayRoundOutcome.textContent = "You Win! Paper beats Rock";
 		return "playerWins";
 	} else if (player === "Scissors" && computer === "Paper") {
-		displayResult.textContent = "You Win! Scissors beats Paper";
+		displayRoundOutcome.textContent = "You Win! Scissors beats Paper";
 		return "playerWins";
 	}
 
 	// computer wins
 	else {
-		displayResult.textContent = `You lose! ${computer} beats ${player}`;
+		displayRoundOutcome.textContent = `You lose! ${computer} beats ${player}`;
 		return "computerWins";
 	}
 }
