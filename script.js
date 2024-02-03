719cacaf6d4b0bddd3d2d28179a1f93c558ef74d
Arinze Ogbuniba <niklaus.oa@gmail.com>
2024-02-03 09:30:58 +0100

    Replay a round when there is a tie

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-btnGroup = document.querySelectorAll("button");
+const btnGroup = document.querySelectorAll("button");
+let playerScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
 	const choice = ["Rock", "Paper", "Scissors"];
@@ -9,42 +11,35 @@ function getComputerChoice() {
 }
 
 function game(e) {
-	let playerScore = 0;
-	let computerScore = 0;
-
 	let playerSelection = e.target.textContent;
 	console.log(playerSelection);
 	let computerSelection = getComputerChoice();
 	let result = playRound(playerSelection, computerSelection);
 
-	// 	// determine who wins each round
-	// 	if (result === "playerWins") {
-	// 		playerScore++;
-	// 		console.log(`player: ${playerScore}
-	//     computer: ${computerScore}
-	//     `);
-	// 	} else {
-	// 		computerScore++;
-	// 		console.log(`player: ${playerScore}
-	//     computer: ${computerScore}
-	//     `);
-	// 	}
+	// determine who wins each round
+	if (result === "playerWins") {
+		playerScore++;
+		console.log(`player: ${playerScore} computer: ${computerScore}`);
+	} else {
+		computerScore++;
+		console.log(`player: ${playerScore} computer: ${computerScore}`);
+	}
 
 	// determine winner
-	if (playerScore > computerScore) {
-		console.log(`Player Wins with ${playerScore} rounds won out of 5`);
-	} else {
-		console.log(`Computer Wins with ${computerScore} rounds won out of 5`);
-	}
+	// if (playerScore > computerScore) {
+	// 	console.log(`Player Wins with ${playerScore} rounds won out of 5`);
+	// } else {
+	// 	console.log(`Computer Wins with ${computerScore} rounds won out of 5`);
+	// }
 }
 
 function playRound(player, computer) {
 	// if tie, replay the round
 	if (player === computer) {
 		console.log(`you both chose ${player}. Replay round`);
-		player = getPlayerChoice();
-		computer = getComputerChoice();
-		return playRound(player, computer);
+		btnGroup.forEach((btn) => {
+			btn.addEventListener("click", game);
+		});
 	}
 
 	// if player wins
